84d0a0147e25cb60ca006bede34d3198bba9da87
im-gitking <avipal577@gmail.com>
2024-01-25 02:02:22 +0530

    Union Types

--- app.js
+++ app.js
@@ -2,11 +2,22 @@ var num1Element = document.getElementById('num1');
 var num2Element = document.getElementById('num2');
 var buttonElement = document.querySelector('button');
 function add(num1, num2) {
-    return num1 + num2;
+    // check both numbers, then pass
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    // check both strings, then pass
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    // number, string or any types, then typecast to number and add
+    return +num1 + +num2;
 }
 buttonElement.addEventListener('click', function () {
     var num1 = num1Element.value;
     var num2 = num2Element.value;
-    var result = add(+num1, +num2); //type casting to number
+    var result = add(+num1, +num2); // type casting to number
+    var resultString = add(num1, num2); // string
     console.log(result);
+    console.log(resultString);
 });
--- app.ts
+++ app.ts
@@ -2,13 +2,24 @@ const num1Element = document.getElementById('num1') as HTMLInputElement;
 const num2Element = document.getElementById('num2') as HTMLInputElement;
 const buttonElement = document.querySelector('button')!;
 
-function add(num1: number, num2: number) {
-    return num1 + num2;
+function add(num1: number | string, num2: number | string) {
+    // check both numbers, then pass
+    if(typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    } 
+    // check both strings, then pass
+    else if(typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + ' ' + num2;
+    }
+    // number, string or any types, then typecast to number and add
+    return +num1 +  +num2;
 }
 
 buttonElement.addEventListener('click', () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
-    const result = add(+num1, +num2);  //type casting to number
+    const result = add(+num1, +num2);      // type casting to number
+    const resultString = add(num1, num2);  // string
     console.log(result);
+    console.log(resultString);
 });
